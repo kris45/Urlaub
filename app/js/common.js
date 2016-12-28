39065e9cc000ad17d13f0a7dfc4b6a62f4f29dd8
kris45 <hristina.salo.94@mail.ru>
2016-12-28 13:25:46 +0200

    urlaub

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,78 @@
-$(function(){
+console.log('hello')
+$(document).ready(function () {
 
-});
+	var pageNumber;
+	var minPerPage = 3;
+	var maxPerPage = 200;
+	var perPage = 20;
+	var cardIndex = 0;
 
+	function doSearch(query, pageNumber, perPage) {
+
+		//console.log('Searching for ' + query + " with pageNumber " + pageNumber + " with perPage " + perPage);
+		var perPageQuery = Math.min(maxPerPage, Math.max(minPerPage, perPage));
+
+		$.ajax({
+	        url: 'https://pixabay.com/api/?key=3750037-59b70644a0ea3a4147bb2a2ad&image_type=photo&per_page=' + perPageQuery + '&q=' + query + '&page=' + pageNumber,
+	        dataType: 'json',
+	        success: function (data) {
+				var hitsCount = data.totalHits;
+				var hits = data.hits;
+				console.log('Found images by searching for ' + query + " : " + hitsCount);
+				for (var i = 0; i < Math.min(perPage, hits.length); i++) {
+					cardIndex++;
+					if (perPage == 1 ) {
+						$(".ideas-block__searchResult").append("<a class='pic__block pic__block--" + cardIndex + "' href='" + hits[i].pageURL + "' data-title='" + query + "' ><img src='" + hits[i].webformatURL +"'/></a>");
+					}
+					else {
+						$(".ideas-block__searchResult").append("<a class='pic__block pic__block--" + cardIndex + "' href='" + hits[i].pageURL + "' data-title='" + hits[i].tags + "' ><img src='" + hits[i].webformatURL +"'/></a>");
+					}
+				}
+
+	        },
+			error: function(xhr){
+				console.log('Error');
+				console.log(xhr);
+			}
+	      });
+
+	};
+
+	$(".searchBtn").click(function (e) {
+		e.preventDefault();
+		var query = $(".searchTxt").val();
+
+		pageNumber = 1;
+		cardIndex = 0;
+		$(".ideas-block__searchResult").empty();
+		doSearch(query, 1, 7);
+		setTimeout(function () {
+			console.log("Doing masonry");
+			$('.ideas-block__searchResult').masonry('reloadItems', {
+				columnWidth: 200,
+				itemSelector: '.pic__block',
+				isFitWidth: true
+			}).masonry();
+			console.log("Finished doinng masonry");
+		}, 1000);
+	});
+
+	var firstSearchData = ["Sport and Activity", "Wellness and Health", "Extreme Sports and Expeditions",
+		"Games", "Culture and Education", "Relaxation", "Travelling"];
+
+	for (var j = 0; j < firstSearchData.length; j++) {
+		var query = firstSearchData[j];
+		doSearch(query, 1, 1);
+	}
+
+	setTimeout(function () {
+		console.log("Doing masonry");
+		$('.ideas-block__searchResult').masonry({
+			itemSelector: '.pic__block',
+			gutter: 20,
+			isFitWidth: true
+		});
+		console.log("Finished doinng masonry");
+	}, 1000);
+
+})
--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,69 @@
-var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	browserSync = require('browser-sync'),
-	concat = require('gulp-concat'),
-	uglify = require('gulp-uglifyjs'),
-	cssnano = require('gulp-cssnano'),
-	rename = require('gulp-rename'),
-	del = require('del');
+var gulp 		 = require('gulp'),
+	sass 		 = require('gulp-sass'),
+	browserSync  = require('browser-sync'),
+	concat 		 = require('gulp-concat'),
+	uglify 		 = require('gulp-uglifyjs'),
+	cssnano 	 = require('gulp-cssnano'),
+	rename 		 = require('gulp-rename'),
+	del 		 = require('del'),
+	autoprefixer = require('gulp-autoprefixer');
+	spritesmith  = require('gulp.spritesmith');
+
+gulp.task('sprite-clean', function() {
+    return del.sync('app/img/sprite*.png');
+});
 
 
+gulp.task('sprite', ['sprite-clean'], function() {
+    var spriteData = gulp.src('app/img/icons/*.png')
+        .pipe(spritesmith({
+            imgName: 'sprite.png',
+            cssName: '_sprite.scss',
+            cssFormat: 'scss',
+            padding: 2,
+            cssVarMap: function (sprite) {
+                sprite.name = 'icon-' + sprite.name
+            },
+            imgPath: '../img/sprite.png'
+        }));
+
+    spriteData.img
+        .pipe(gulp.dest('app/img/'));
+
+    spriteData.css
+        .pipe(gulp.dest('app/sass/'));
+
+    return spriteData;
+});
 
 gulp.task('sass', function(){
 
 	return gulp.src('app/sass/**/*.scss')
 	.pipe(sass())
+	.pipe(autoprefixer(['last 15 versions', '>1%', 'ie 8'], {cascade: true}))
 	.pipe(gulp.dest('app/css'))
 	.pipe(browserSync.reload({stream: true}))
 });
 
+
 gulp.task('scripts', function(){
 
 	return gulp.src([
-		'app/libs/jquery/dist/jquery.min.js',
-		'app/libs/masonry/dist/masonry.pkgd.min.js'])
-		.pipe(concat('libs.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('app/js'));
+        'app/libs/tmpl/tmpl.js',
+        'app/libs/masonry/masonry.pkgd.js',
+        'app/libs/masonry/imagesloaded.pkgd.js'
+    ])
+	.pipe(concat('libs.min.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('app/js'));
+});
+
+gulp.task('ie-libs', function() {
+
+    return gulp.src('app/libs/html5shiv/html5shiv.js')
+    .pipe(concat('ie.libs.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js'));
 });
 
 gulp.task('cssmin', ['sass'], function(){
@@ -49,13 +88,11 @@ gulp.task('clean', function(){
 	return del.sync('dist');
 })
 
-
-gulp.task('watch', ['browser-sync', 'cssmin', 'scripts'], function(){
+gulp.task('watch', ['browser-sync', 'cssmin', 'scripts', 'ie-libs'], function(){
 	gulp.watch('app/sass/**/*.scss', ['sass']);
 	gulp.watch('app/*.html', browserSync.reload);
 	gulp.watch('app/**/*.js', browserSync.reload);
 });
-
 
 gulp.task('build', ['clean', 'sass', 'scripts'], function(){
 
